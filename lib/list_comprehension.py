030fd113c9c0086a96d10dbd4ceec3964b8f84c8
byron korir <byron.korir@student.moringaschool.com>
2023-11-22 19:10:32 +0300

    assignment complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    for n in num_list:
+        
+        if n % 2 != 0:
+            return []
+        else:
+            return [n for n in num_list if n % 2 == 0]           
     pass
 
 def make_exclamation(sentence_list):
+    if len(sentence_list) == 0 :
+        return []
+    else:
+        return [s + '!' for s in sentence_list]
+    
+         
     pass
